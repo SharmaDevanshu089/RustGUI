ea0f0382a00c81b258103b8054de603872268398
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-08-12 23:29:42 +0530

    Added a Variable

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use winit::{event::{self, Event}, event_loop::{self, EventLoop}, window::WindowBuilder};
 
 fn main() {
+    let num = 4;
     let event_loop = EventLoop::new().unwrap();
     let window = WindowBuilder::new()
     .with_title("New Window")
@@ -8,7 +9,7 @@ fn main() {
     .unwrap();
     event_loop.run(move | event , elwt| 
         {
-            match event {
+            match num {
                 
             }
         }).unwrap();
